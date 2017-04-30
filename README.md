9f1a1996c6231061912a234c284a6ba968821c04
Srujun Thanmay Gupta <theununtrium@gmail.com>
2017-04-29 19:13:28 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Distributed Transactions
+========================
+
+NetIDs: sgupta80 banerj10
+
+**IMPORTANT**: Needs Python 3.6 to run.
---
+++ fabfile.py
@@ -0,0 +1,18 @@
+from fabric.api import *
+
+s = 'sp17-cs425-g15-{:02d}.cs.illinois.edu'
+
+env.hosts = [s.format(x) for x in range(1, 11)]
+env.user = 'sgupta80'
+
+def deploy():
+    with cd('~/mp3'):
+        run('git pull')
+
+def git_clone():
+    with cd('~'):
+        run('git clone git@gitlab.engr.illinois.edu:sgupta80/ece428-mp3.git mp3')
+
+def install_py36():
+    run('sudo yum -y install https://centos7.iuscommunity.org/ius-release.rpm')
+    run('sudo yum -y install python36u python36u-pip')
